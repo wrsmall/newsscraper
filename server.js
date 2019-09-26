be0b4ccb9a8dcb3b821ed26dfd53882bdde9f08c
wrsmall <william.r.small89@gmail.com>
2019-09-26 18:17:45 -0400

    working app

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect(MONGODB_URI), { useNewUrlParser: true });
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 // Routes
 var results = [];
